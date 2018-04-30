7a5313328626ac914ab9c6a149b7b861960b0593
Sean Huang <hyprul@ucla.edu>
2018-04-30 01:34:07 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,1 +1,11 @@
 var playlist = {Hodor: "GOT"};
+
+function updatePlaylist(object, key, title){
+  object.key = title
+  return object
+}
+
+function removeFromPlaylist(object, key){
+  delete object.key
+  return object
+}
